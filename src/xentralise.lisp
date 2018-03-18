e1b29aff148d54a92c10e49a12007d2356e7983a
Selwyn Simsek <sgs16@ic.ac.uk>
2018-03-18 18:25:04 +0000

    Added code to establish the SSH tunnel on linux and OS X

--- src/xentralise.lisp
+++ src/xentralise.lisp
@@ -1,5 +1,25 @@
 (defpackage xentralise
-  (:use :cl))
+  (:use #:cl)
+  (:export '(#:connected-p #:start #:start-server #:stop #:stop-server #:evaluate-on-lisp)))
 (in-package :xentralise)
 
 ;; blah blah blah.
+
+(defparameter *current-port* nil)
+
+(defun start (&key (port 4005) (server-host-name "localhost") ())
+  "Starts a SWANK client that listens for connections on the given port, and establishes a SSH tunnel into the server."
+  (swank:create-server :port (setf current-port port) :dont-close t)
+  (establish-ssh-tunnel server-host-name))
+
+(defun stop ()
+  "Stops the currently running "
+  (swank:stop-server *current-port*)
+  (setf *current-port* nil))
+
+(defun connected-p ()
+  "Whether the Xentralise client is running."
+  *current-port*)
+
+(defun start-server ()
+  "Starts a Xentralise server that listens for connections on the specified port.")
