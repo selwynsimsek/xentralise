f48db42682e1444bbf0fd691041fefd11ca94e5e
Selwyn Simsek <sgs16@ic.ac.uk>
2018-03-18 18:28:00 +0000

    Added code to establish the SSH tunnel on linux and OS X

--- src/xentralise.lisp
+++ src/xentralise.lisp
@@ -6,11 +6,19 @@
 ;; blah blah blah.
 
 (defparameter *current-port* nil)
+(defparameter *current-server* nil)
+ 
+(defun establish-ssh-tunnel (server-host-name port)
+  "Establishes the remote SSH tunnel into the Xentralise server at server-host-name."
+  #+windows
+  (format t "Windows not yet supported.")
+  #+ (or linux darwin)
+  (uiop:run-program (format nil "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -N -f ~A -R~D:localhost:~D" server-host-name port port)))
 
-(defun start (&key (port 4005) (server-host-name "localhost") ())
+(defun start (&key (port 4005) (server-host-name "localhost"))
   "Starts a SWANK client that listens for connections on the given port, and establishes a SSH tunnel into the server."
-  (swank:create-server :port (setf current-port port) :dont-close t)
-  (establish-ssh-tunnel server-host-name))
+  (swank:create-server :port (setf *current-port* port) :dont-close t)
+  (establish-ssh-tunnel server-host-name port))
 
 (defun stop ()
   "Stops the currently running "
@@ -21,5 +29,16 @@
   "Whether the Xentralise client is running."
   *current-port*)
 
-(defun start-server ()
-  "Starts a Xentralise server that listens for connections on the specified port.")
+(defun start-server (&optional (range '(4000 5000)))
+  "Starts a Xentralise server that listens for connections in the specified port range."
+  (let ((start (car range))
+        (end (cadr range)))
+    ))
+
+(defun handle-server-connections ())
+
+(defun server-running-p ()
+  "Whether the server is running or not.")
+
+(defun stop-server ()
+  "Stops the Xentralise server.")
